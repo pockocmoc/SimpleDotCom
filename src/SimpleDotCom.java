6f127cdb49074e810204b4e36326850bef9cfb1e
pockocmoc <pockocmoc@inbox.ru>
2022-07-07 13:11:38 +0300

    0.1

--- src/SimpleDotCom.java
+++ src/SimpleDotCom.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class SimpleDotCom {
+public class SimpleDotCom {
+    int[] loctionCells;
+    int numOfHits = 0;
+
+    public void setLoctionCells(int[] locs) {
+        loctionCells = locs;
+    }
+    public String checkYourself(String stringGuess) {
+        int guess = Integer.parseInt(stringGuess);
+        String result = "Мимо";
+        for (int cell : loctionCells) {
+            if (guess == cell) {
+                result = "Попал";
+                numOfHits++;
+                break;
+            }
+        }
+        if (numOfHits == loctionCells.length) {
+            result = "Потопил";
+        }
+        System.out.println(result);
+        return result;
+    }
 }
--- src/SimpleDotComGame.java
+++ src/SimpleDotComGame.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class SimpleDotComGame {
+public class SimpleDotComGame {
+    public static void main(String[] args) {
+        int numOfGuess = 0;
+        GameHelper helper = new GameHelper();
+
+        SimpleDotCom theDotCom = new SimpleDotCom();
+        int randomNum = (int) (Math.random() * 5);
+
+        int[] loctions = {randomNum, randomNum+1, randomNum+2};
+        theDotCom.setLoctionCells(loctions);
+        boolean isAlive = true;
+
+        while (isAlive == true) {
+            String guess = helper.getUserInput("Введите число");
+            String result = theDotCom.checkYourself(guess);
+            numOfGuess++;
+
+            if (result.equals("Потопил")) {
+                isAlive = false;
+                System.out.println("Вам понадобилось " +numOfGuess+ " попыток(и)");
+            }
+        }
+    }
 }
